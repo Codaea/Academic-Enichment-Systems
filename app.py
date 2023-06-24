008dc501ad5ba570f30c9dfacad53d233aa47863
Codaea <93889672+Codaea@users.noreply.github.com>
2023-06-24 01:58:52 -0700

    fixed perms (again) and login logout functonality

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-import sqlite3
-from functools import wraps
-from flask import Flask, request, jsonify, make_response, render_template
-import jwt
+from flask import Flask, request, jsonify, make_response, render_template, session, redirect, url_for
+import sqlite3 # databasing software
+from functools import wraps # decorators
+import jwt # auth
+from datetime import datetime, timedelta # sets jwt expiry
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
@@ -13,8 +15,8 @@ cursor = conn.cursor()
 
 # User roles and their corresponding permissions
 ROLES = {
-    'root': ['read requests', 'write requests', 'gen requests', 'gen reports', 'edit db', 'edit config'],
-    'admin': ['read requests', 'write requests', 'gen requests', 'gen reports', 'edit db', 'edit config'],
+    'root': ['read requests', 'write requests', 'gen requests', 'gen reports', 'edit student db', 'edit config'],
+    'admin': ['read requests', 'write requests', 'gen requests', 'gen reports', 'edit student db', 'edit config'],
     'eUser': ['read requests', 'write requests', 'gen requests', 'gen reports', 'read reports'],
     'attendance': ['read reports', 'gen reports'],
     'user': ['read requests', 'write requests']
@@ -43,7 +45,7 @@ def create_jwt(username, role):
     payload = {
         'username': username,
         'role': role,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=2) #change for login expiry time
+        'exp': datetime.utcnow() + timedelta(minutes=5)  # Expiration time: 1 hour from now
     }
     token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
     return token
@@ -58,6 +60,7 @@ def decode_jwt(token):
         payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
         return payload
     except jwt.ExpiredSignatureError:
+        print("session expired. please try again.")
         return None
 
 
@@ -68,29 +71,42 @@ def has_permission(permission):
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            token = request.headers.get('Authorization')
-            if token:
-                token = token.split('Bearer ')[1]
-                payload = decode_jwt(token)
+            session_token = session.get('token')
+            if session_token:
+                payload = decode_jwt(session_token)
                 if payload and 'role' in payload:
                     role = payload['role']
                     if permission in ROLES.get(role, []):
                         return f(*args, **kwargs)
-            return jsonify({'message': 'Unauthorized'}), 401
+            return 'Unauthorized. Please contact your network administrator if you think there has been a mistake.'
         return wrapper
     return decorator
 
 # app routes
 
+#home route
 @app.route('/', methods=['GET'])
 def home():
-    print('hmpage called')
-    return 'Home page'
+    token = session.get('token') 
+    if token: # checks to see if there even is a token
+       if decode_jwt(token=token): # checks for any jwt errors. if any, it returns none from
+           #put any code that you want to run when verifyed here on homepage
+
+           #need to add auto send to right panel based on if admin or teacher
+
+           
+           return 'all good!'
+       
+    return redirect('/login')
+
+
+
+
 # login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """
-    Renders the login page and authenticates the user.
+    Renders the login page and authenticates the user and returns a JWT token.
     """
     if request.method == 'POST':
         # Handle form submission
@@ -99,8 +115,14 @@ def login():
 
         # Authentication logic
         if authenticate(username, password):
-            # User authenticated successfully, perform further actions
-            return "User authenticated successfully!"
+            role = authenticate(username, password)
+            if role:
+                token = create_jwt(username, role)
+                decode_jwt(token=token)
+                session['token'] = token  # Save the JWT token to the session
+                response = 'Login Sucessfull! Token is ' + token
+                return response
+                
         else:
             error_message = "Invalid credentials. Please try again."
 
@@ -108,6 +130,14 @@ def login():
     else:
         # Render the login page
         return render_template('login.html', error_message='')
+
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    if session.get('token'):
+        session.pop('token', None)
+        return "logged out!"
+    else:
+        return "You where never logged in!"
 
 # teacher requesting landing page
 @app.route('/erequests', methods=['GET'])
@@ -131,9 +161,21 @@ def attendancereport():
 
 # admin panel, controlls all the student scedules, teacher admins
 @app.route('/admin', methods=['GET', 'POST'])
+@has_permission('edit student db')
 def admin():
-    
     return 'Admin Panel'
+    if request.method == 'POST':
+        file = request.files['csv_file']
+        if file.filename == '':
+            return 'No file selected!'
+        if file:
+            df = pd.read_csv(file)
+            connect = sqlite3.connect('database.db')
+            df.to_sql('masterschedule', connect, if_exists='replace', index=False)
+            conn.close()
+            return 'CSV file uploaded and saved to database!'
+    return render_template('upload.html')
+    
 
 
 if __name__ == '__main__':
