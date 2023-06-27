fb3023faa86ff819787df9c485cd733880656a0f
Codaea <93889672+Codaea@users.noreply.github.com>
2023-06-27 16:26:36 -0700

    removing old code

--- requestsquery.sql
+++
@@ -1,8 +0,0 @@
--- SQLite
-SELECT masterschedule.*, (
-            SELECT requests.requested_rooms
-            FROM requests
-            WHERE requests.studentId = masterschedule.studentId
-            ) AS matched_value
-            FROM masterschedule
-            WHERE masterschedule.period1 = 103;
